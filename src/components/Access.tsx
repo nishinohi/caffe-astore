d39637c0168cb8a6d3f1389ed033ddbbafb24478
azone-nishi <tomoya.nishi@azone-japan.co.jp>
2025-09-13 01:32:11 +0900

    add: access 追加

---
+++ src/components/Access.tsx
@@ -0,0 +1,53 @@
+import Logo from '@/components/Logo'
+
+export default function Access() {
+  return (
+    <article className="relative mx-auto max-w-7xl px-4 py-16 md:px-6 md:py-24">
+      <h2 className="font-josefin mb-12 text-center text-5xl md:mb-16 md:text-6xl">Access</h2>
+      <div className="flex flex-col items-center space-y-6 md:space-y-12">
+        <img
+          className="h-full max-h-[500px] w-full rounded-2xl object-cover object-center shadow-lg"
+          src="/caffe-astore/assets/others/shop.png"
+          alt="Caffè Astore 店内写真"
+        />
+
+        <div className="flex flex-col items-center space-y-4">
+          <Logo className="text-foreground h-auto w-48 max-w-[90vw]" />
+          <p className="text-base md:text-lg">
+            <span className="pr-2">カフェ</span>アストーレ
+          </p>
+          <a
+            href="https://maps.app.goo.gl/ZYWGP86SaL5MYLUKA"
+            className="inline-flex items-center border-b border-neutral-400 pb-1 text-base transition-colors hover:border-neutral-800 md:text-lg"
+            target="_blank"
+          >
+            大阪府中央区玉造2-1-3
+          </a>
+          <p className="text-base md:text-lg">
+            12:00 〜 20:00<span className="px-4">/</span>日・月<span className="pl-4">定休</span>
+          </p>
+          <div className="flex flex-col gap-4 pt-6 md:flex-row md:gap-6">
+            <a
+              href="https://note.com/caffeastore"
+              className="inline-flex items-center border-b border-neutral-400 pb-1 text-lg transition-colors hover:border-neutral-800"
+              target="_blank"
+            >
+              note
+            </a>
+            <a
+              href="https://www.instagram.com/caffeastore2019"
+              className="inline-flex items-center border-b border-neutral-400 pb-1 text-lg transition-colors hover:border-neutral-800"
+              target="_blank"
+            >
+              instagram
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <footer className="mt-16 text-center">
+        <p className="text-sm">© Caffè Astore. All RIGHTS RESERVED.</p>
+      </footer>
+    </article>
+  )
+}
